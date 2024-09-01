4efc33ded7b792fcceb89ecfbd1967e64c406b81
Swiss Tangsatjatham <tintinguy.01@gmail.com>
2024-09-01 16:15:00 +0700

    update robo-friends

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
+import CardList from "./CardList";
+import { robots } from "./robot";
+import SearchBox from "./SearchBox";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value });
+    }
+    render () {
+        const filterRobots = this.state.robots.filter(robots => {
+            return robots.name.toLocaleLowerCase().includes(this.state.searchfield.toLocaleLowerCase());
+        });
+        return (
+            <div className="tc">
+                <h1 className="f1">RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filterRobots} />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/robot.js
@@ -0,0 +1,32 @@
+export const robots = [
+    {
+        id: 1,
+        name: "Johny Johnson",
+        username: 'johnnyjohn',
+        email: "johny_john@gmail.com"
+    },
+    {
+        id: 2,
+        name: "Benjamin Franks",
+        username: 'benjaminfranks',
+        email: "ben_benjamin@gmail.com"
+    },
+    {
+        id: 3,
+        name: "Gwen Laura",
+        username: 'gwenny',
+        email: "gwennygwen@gmail.com"
+    },
+    {
+        id: 4,
+        name: "Kelvin Kernell",
+        username: 'kelvin11',
+        email: "kelvinthekernell@gmail.com"
+    },
+    {
+        id: 5,
+        name: "Thomas Brook",
+        username: 'brookybrook',
+        email: "tommybrook@gmail.com"
+    },
+];
