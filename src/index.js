a7153697464c38b8c76304737a119b8196081301
Swiss Tangsatjatham <tintinguy.01@gmail.com>
2024-09-02 19:19:27 +0700

    added redux in project

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
 import reportWebVitals from './reportWebVitals';
+import { searchRobots, requestRobots } from './reducer';
+import { createLogger } from 'redux-logger';
+import { thunk } from 'redux-thunk';
+import { combineReducers } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+
+const logger = createLogger();
+
+const rootReducers = combineReducers({ searchRobots, requestRobots })
+
+const store = configureStore({
+  reducer: rootReducers,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk, logger)
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
